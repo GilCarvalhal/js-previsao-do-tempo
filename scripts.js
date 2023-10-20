d581309ae2e768501778c16df2d2dfb003d5e718
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-10-20 00:31:57 -0300

    refactor: implementacao das funcoes

--- scripts.js
+++ scripts.js
@@ -2,12 +2,31 @@
 
 const key = "e764472f4f9a8ae4d738ed734613667b";
 
+function colocarDadosNaTela(dados) {
+  console.log(dados);
+
+  document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name;
+
+  document.querySelector(".temp").innerHTML =
+    Math.floor(dados.main.temp) + "°C";
+
+  document.querySelector(".texto-previsao").innerHTML =
+    dados.weather[0].description;
+
+  document.querySelector(".umidade").innerHTML =
+    "Umidade: " + dados.main.humidity + "%";
+
+  document.querySelector(
+    ".img-previsao"
+  ).src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
+}
+
 async function buscarCidade(cidade) {
   const dados = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`
   ).then((response) => response.json());
 
-  console.log(dados);
+  colocarDadosNaTela(dados);
 }
 
 function buttonClick() {
